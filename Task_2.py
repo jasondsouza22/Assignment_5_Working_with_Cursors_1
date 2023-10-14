a8841b8e8ebed7b16a9b4eb63c167d545575f7be
jasondsouza22 <jasondsouza2211@gmail.com>
2023-10-14 16:40:12 +0530

    Task_2

---
+++ Task_2.py
@@ -0,0 +1,25 @@
+import arcpy
+import os
+
+gdb_path = r"D:\Second Year\Sem 3\Programming_for_GIS_III\Assignments\Assignment_5_Working_with_Cursors_1\ProProject_Cursors\ProProject_Cursors.gdb"
+fc_name = "MajorAttractions"
+fc_path = os.path.join(gdb_path,fc_name)
+
+After_1970 =[]
+
+field_list = ["NAME","ESTAB"]
+
+search_cursor = arcpy.da.SearchCursor(fc_path,field_list)
+
+for row in search_cursor:
+    if row[1] > 1970:
+        After_1970.append(row[0])
+
+for name in After_1970:
+    print("Major Attraction Established After 1970:", name)
+
+
+
+
+
+
